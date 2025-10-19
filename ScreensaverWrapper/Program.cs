542d2c23d5dfb1143c607202ce312fd355aa4ece
Queazified <cohaiandrey@gmail.com>
2025-10-19 16:10:58 +0000

    Updating files.

--- ScreensaverWrapper/Program.cs
+++ ScreensaverWrapper/Program.cs
@@ -2,12 +2,15 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
 
 class Program
 {
+    [STAThread]
     static void Main()
     {
-        Console.WriteLine("Starting screensaver wrapper...");
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
         
         // Path to the screensaver file relative to the EXE
         string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "screensaver", "Screensaver.scr");
@@ -28,11 +31,14 @@ class Program
                 p.StartInfo.FileName = path;
                 p.StartInfo.Arguments = "/s";  // Run screensaver in full-screen mode
                 p.StartInfo.UseShellExecute = true;
+                p.StartInfo.Verb = "runas"; // Run as administrator
                 p.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
-                p.StartInfo.CreateNoWindow = false;
-
+                
                 Console.WriteLine("Launching screensaver...");
                 p.Start();
+
+                // Hide console window
+                ShowWindow(GetConsoleWindow(), SW_HIDE);
 
                 // Wait until the screensaver exits
                 p.WaitForExit();
@@ -48,4 +54,12 @@ class Program
             }
         }
     }
+
+    [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    const int SW_HIDE = 0;
 }
---
+++ ScreensaverWrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Out of Order Screensaver Wrapper")]
+[assembly: AssemblyDescription("Wrapper for Out of Order Screensaver")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Queazified.co.uk")]
+[assembly: AssemblyProduct("Out of Order Screensaver Wrapper")]
+[assembly: AssemblyCopyright("Copyright © Queazified.co.uk 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("YOUR-GUID-HERE")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
